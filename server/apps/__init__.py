79f63fe39b9e997bea1ba6fb1b21d0bfc8214b00
cenjinhe <cenjinhe@126.com>
2023-10-11 22:43:50 +0800

    del server

--- server/apps/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author   : jinhe Cen
-# @Time     : 2023/6/10 16:19
-# @File     : __init__.py.py
-
-
--- server/launch.sh
+++
@@ -1,5 +0,0 @@
-# 先停止gunicorn
-/flaskapp_server$ sudo kill $(lsof -i:5000|awk '{if(NR==2)print $2}')
-
-# 再启动gunicorn
-/flaskapp_server$ gunicorn --config=gunicorn_config.py main:app
