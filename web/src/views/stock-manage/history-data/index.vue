66d11b89ca32ae437d49979a1f7d626ce495ecb7
cenjinhe <cenjinhe@126.com>
2023-10-15 22:00:49 +0800

    update

---
+++ web/src/views/stock-manage/history-data/index.vue
@@ -0,0 +1,347 @@
+<template>
+  <pro-table
+    ref="table"
+    :title="$t('test/list.title')"
+    :request="getList"
+    :columns="columns"
+    :search="searchConfig"
+    :default-sort="{ prop: 'code', order: 'ascending' }"
+    @selectionChange="handleSelectionChange"
+  >
+    <!-- 工具栏 -->
+    <template #toolbar>
+      <el-button type="primary" @click="updateStockList">
+        更新列表
+      </el-button>
+      <el-button type="primary" @click="updateHistoryDataSZ">
+        更新深市数据(all)
+      </el-button>
+      <el-button type="primary" disabled>
+        更新泸市数据(all)
+      </el-button>
+      <el-button icon="Refresh" @click="refresh">
+        刷新
+      </el-button>
+    </template>
+    <template #status="{row}">
+      <el-tag :type="row.status === 0 ? 'success' : 'error'">
+        {{ row.status === 0 ? '已更新' : '更新中' }}
+      </el-tag>
+    </template>
+    <template #operate="scope">
+      <el-button size="small" type="primary" @click="btnUpdateData(scope.row)">
+        更新数据
+      </el-button>
+      <el-button size="small" type="danger" @click="btnDeleteRow(scope.row, scope.$index)">
+        删除
+      </el-button>
+      <el-button size="small" type="primary" @click="btnChangeStatus(scope.row)">
+        修改状态
+      </el-button>
+    </template>
+  </pro-table>
+</template>
+
+<script>
+import { defineComponent, reactive, ref, toRefs } from 'vue'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import {
+  getStockList,
+  getStatusList,
+  updateStockList,
+  updateStatus,
+  deleteStockRecord,
+  update_history_data,
+  update_history_data_sz,
+} from '@/api/stock-manage'
+
+export default defineComponent({
+  // name: 'historyData',
+  created(){
+    console.log('created')
+  },
+  setup() {
+    // const { proxy } = getCurrentInstance()
+
+    const state = reactive({
+      // 表格列配置，大部分属性跟el-table-column配置一样
+      columns: [
+        { type: 'selection', width: 56 },
+        { label: 'test/list.index', type: 'index', width: 80 },
+        {
+          label: 'A股代码',
+          prop: 'code',
+          minWidth: 160,
+        },
+        {
+          label: 'A股简称',
+          prop: 'name',
+          minWidth: 160,
+        },
+        {
+          label: '上市日期',
+          prop: 'date',
+          minWidth: 160,
+        },
+        {
+          label: '现价',
+          prop: 'close',
+          minWidth: 100,
+        },
+        {
+          label: '更新状态',
+          prop: 'status',
+          minWidth: 100,
+          tdSlot: 'status',
+        },
+        {
+          label: '更新时间',
+          prop: 'update_time',
+          minWidth: 160,
+        },
+        {
+          label: '操作',
+          width: 260,
+          align: 'center',
+          tdSlot: 'operate', // 自定义单元格内容的插槽名称
+        },
+      ],
+      // 搜索配置
+      searchConfig: {
+        labelWidth: '90px', // 必须带上单位
+        inputWidth: '180px', // 必须带上单位
+        fields: [
+          {
+            type: 'text',
+            label: 'A股代码',
+            name: 'code',
+            defaultValue: '',
+          },
+          {
+            type: 'text',
+            label: 'A股简称',
+            name: 'name',
+            defaultValue: '',
+          },
+          {
+            label: '更新状态',
+            name: 'status',
+            type: 'select',
+            defaultValue: 2,
+            options: [
+              {
+                name: '全部',
+                value: 2,
+              },
+              {
+                name: '更新中',
+                value: 1,
+              },
+              {
+                name: '已更新',
+                value: 0,
+              },
+            ],
+          },
+          {
+            label: '证券交易所',
+            name: 'market',
+            type: 'radio',
+            defaultValue: 1,
+            options: [
+              {
+                name: '深市',
+                value: 1,
+              },
+              {
+                name: '沪市',
+                value: 0,
+              },
+            ],
+          },
+        ],
+      },
+      // 选择
+      selectedItems: [],
+      handleSelectionChange(arr) {
+        state.selectedItems = arr
+      },
+      // 请求函数
+      async getList(params) {
+        // params是从组件接收的，包含分页和搜索字段。
+        const { data } = await getStockList(params)
+        // 必须要返回一个对象，包含data数组和total总数
+        return { data: data.list, total: data.total }
+      },
+      // 【更新列表】按钮
+      async updateStockList() {
+        const market = table.value.searchModel.market
+        const exchange = market === 1 ? '深市' : '泸市'
+        ElMessageBox.confirm(`更新${exchange}A股列表, 是否继续?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        }).then(async () => {
+          const market = table.value.searchModel.market
+          const param = { market: market }
+          await updateStockList(param)
+          // refresh()
+          setTimer()
+          ElMessage({ type: 'success', message: '更新成功' })
+        })
+      },
+      // 【更新深市数据(all)】按钮
+      async updateHistoryDataSZ() {
+        ElMessageBox.confirm(`更新深市数据, 是否继续?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        }).then(async () => {
+          const param = {}
+          await update_history_data_sz(param)
+        })
+      },
+      // 【更新数据)】按钮
+      async btnUpdateData(row) {
+        ElMessageBox.confirm(
+          `更新A股代码${row.code}历史数据, 是否继续?`,
+          '提示',
+          {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning',
+          }
+        ).then(async () => {
+          const market = table.value.searchModel.market
+          const param = {
+            market: market,
+            code: row.code,
+            release_date: row.date,
+          }
+          const { code, message } = await update_history_data(param)
+          if (code === '200') {
+            setTimer()
+            ElMessage({ type: 'success', message: '更新成功' })
+          } else if (code === '201') {
+            ElMessage({ type: 'warning', message: message })
+          } else {
+            console.log('done')
+          }
+        })
+      },
+      // 【删除)】按钮
+      async btnDeleteRow(row, index) {
+        ElMessageBox.confirm(
+          `这将会删除A股代码[${row.code}], 是否继续?`,
+          '提示',
+          {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning',
+          }
+        ).then(async () => {
+          const market = table.value.searchModel.market
+          const param = { market: market, id: row.id }
+          await deleteStockRecord(param)
+          refresh()
+          ElMessage({ type: 'success', message: '删除成功' })
+        })
+      },
+      // 【切换状态)】按钮
+      async btnChangeStatus(row) {
+        ElMessageBox.confirm(
+          `切换A股代码${row.code}的状态, 是否继续?`,
+          '提示',
+          {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning',
+          }
+        ).then(async () => {
+          const market = table.value.searchModel.market
+          const param = { market: market, code: row.code }
+          const { code } = await updateStatus(param)
+          if (code === '200') {
+            ElMessage({ type: 'success', message: '切换成功' })
+            refresh()
+          } else {
+            console.log('done')
+          }
+        })
+      },
+    })
+    const table = ref(null)
+    const refresh = () => {
+      table.value.refresh()
+    }
+    let timer = ref(0)
+    const setTimer = () => {
+      if (timer.value === 0) {
+        timer.value = setInterval(async () => {
+          await judgeStatus()
+        }, 2000)
+      }
+    }
+    const clearTimer = () => {
+      if (timer.value !== 0) {
+        timer.value = 0
+      }
+    }
+    let status_list = ref([])
+    const judgeStatus = async () => {
+      // 前回状态list
+      let pre_status = status_list
+      // 当前状态list
+      const params = {
+        code: table.value.searchModel.code,
+        name: table.value.searchModel.name,
+        status: table.value.searchModel.status,
+        market: table.value.searchModel.market,
+        current: table.value.pageNum,
+        pageSize: table.value.pageSize,
+      }
+      const { data } = await getStatusList(params)
+      console.log('data=', data)
+      status_list.value = data
+
+      let refreshFlg = false
+      let timerFlg = true
+      console.log('status_list.value=', status_list.value)
+      if (pre_status.length === status_list.value.length) {
+        for (let i = 0; i < status_list.value.length; i++) {
+          // status的值是否有变化
+          if (pre_status[i] != status_list.value[i] && !refreshFlg) {
+            refreshFlg = true
+          }
+          // status的值是否有更新中的状态
+          if (status_list.value[i] === 1 && timerFlg) {
+            timerFlg = false
+          }
+        }
+      }
+      // 刷新table
+      if (refreshFlg) {
+        table.value.refresh()
+      }
+      // 关闭定时器
+      if (!refreshFlg) {
+        clearTimer()
+      }
+    }
+    return {
+      ...toRefs(state),
+      refresh,
+      setTimer,
+      clearTimer,
+      judgeStatus,
+      timer,
+      status_list,
+      table,
+    }
+  },
+  mounted(){
+    console.log('mounted')
+    // setTimer()
+  }
+})
+</script>
