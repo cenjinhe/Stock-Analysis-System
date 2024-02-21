be6bb415f37f7397f1481d0cddc57473d5fa53d8
cenjinhe <cenjinhe@126.com>
2024-02-21 23:01:57 +0800

    update

--- server-django/stock_manage/views_recommend.py
+++ server-django/stock_manage/views_recommend.py
@@ -1,13 +1,5 @@
-import os
 import json
-import pandas as pd
-from datetime import datetime
-from django.db import connection
 from django.http import HttpResponse
-from django.forms import model_to_dict
-from django.core.paginator import Paginator
-from django.http.response import JsonResponse
-from stock_manage import models_sql
 from stock_manage.utils import handler, 拟合斜率, macd
 from stock_manage.models import StockListSZ, StockListSH, StockOnAnalysis
 
@@ -22,24 +14,24 @@ def postUpdateStockRecommend(request):
         for market in [1]:
             # 分别获取[深市, 沪市]的股票列表
             queryset = TABLE_MAP.get(str(market)).objects.all().values()
-            data = list(queryset)
+            stockList = list(queryset)
             # 遍历股票列表
-            # for row in data:
-            for row in data[0:1]:
+            for row in stockList:
                 code = row['code']
+                # 获取原始数据
                 rawData = handler.getRawDataList(code, COUNT)
                 if len(rawData) > 0:
-                    # 获取data，如:[[日期(date), 开盘(open)，收盘(close)，最低(low)，最高(high)]]，并倒序(日期从小到大)
+                    # 分割原始数据 如:[[日期(date), 开盘(open)，收盘(close)，最低(low)，最高(high)]]，并倒序(日期从小到大)
                     data = [[element[0], element[1], element[2], element[3], element[4]] for element in rawData][::-1]
-                    # print('data=', data)
-                    # 计算MACD指标
+
+                    # ---------------计算MACD指标 start---------------
                     macd_data, dif_data, dea_data = macd.calculateMACD(10, 21, 9, data, 2)
                     print('macd_data=', macd_data)
                     print('dif_data=', dif_data)
                     print('dea_data=', dea_data)
+                    # ---------------计算MACD指标 end-----------------
 
-
-        # 3根据code，计算x-1日上次的斜率状态
-        # 4根据code，更新或插入数据
+                # test code
+                break
         return HttpResponse(json.dumps([], ensure_ascii=False), content_type="application/json,charset=utf-8")
 
