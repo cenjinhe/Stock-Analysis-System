a8808dd9a9a1ea74223683759f76358490891669
cenjinhe <cenjinhe@126.com>
2024-03-16 21:50:56 +0800

    update

---
+++ server-django/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : jinhe Cen
+# @Time     : 2023/12/13 21:20
+# @File     : __init__.py.py
+
+
---
+++ server-django/stock_manage/script/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : jinhe Cen
+# @Time     : 2023/10/4 18:49
+# @File     : __init__.py.py
+
+
